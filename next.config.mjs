624719464715062c113dabe95b55e32012d900f2
MiamiGlobalInvestmentsGroupLLC <motazbarakat2007132@gmail.com>
2025-09-06 14:43:10 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  experimental: { serverActions: { allowedOrigins: ['*'] } }
-};
-export default nextConfig;
